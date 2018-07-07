99bebffe2e2d14d3576adef715028b30721affec
hm-yap <haomengyap@gmail.com>
2018-07-07 21:57:35 +0800

    Add lint + move to src

--- js/app.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-const app = express();
-
-app.get('/', (req, res) => res.send('Initial version - root path. TBD manage routes from ./routes'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
